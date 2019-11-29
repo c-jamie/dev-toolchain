c9c40bbb2c80fe8493e1c7dc3b79472c6ee0c663
droairne <droairne@dev>
2019-11-29 10:42:06 +0000

    initial commit

---
+++ linux-bootstrap.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+CODE_DIR=$HOME/code
+ARROW_DIR=$CODE_DIR/arrow
+MINICONDA_DIR=$HOME/miniconda
+TOOLCHAIN_DIR=$HOME/dev-toolchain
+
+function build_user() {
+	adduser droairne
+	usermod -aG sudo droairne
+
+}
+
+# Dotfiles
+function install_dotfiles() {
+    ln -sf $TOOLCHAIN_DIR/dotfiles/bash_personal ~/.bash_personal
+
+	BASHRC_ADDITION=$(cat <<-END
+	if [ -f ~/.bash_personal ]; then
+	    . ~/.bash_personal
+	fi
+	END
+	)
+    echo "$BASHRC_ADDITION" >> ~/.bashrc
+}
+
+# packages
+function install_apt_packages() {
+    sudo apt install -y \
+	    git \
+        filezilla \
+	    tmux \
+        node \
+        yarn
+}
+
+# Miniconda
+function install_conda() {
+    MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+    wget -O miniconda.sh $MINICONDA_URL
+    bash miniconda.sh -b -p $MINICONDA_DIR
+    rm -f miniconda.sh
+    export PATH="$MINICONDA_DIR/bin:$PATH"
+
+    conda update -y -q conda
+    conda config --set auto_update_conda false
+    conda info -a
+
+    conda config --set show_channel_urls True
+    conda config --add channels https://repo.continuum.io/pkgs/free
+    conda config --add channels conda-forge
+}
+
+# Install conda environments
+function create_conda_dev_environments() {
+    conda create -yq -n a37 \
+          python=3.7
+    conda create -yq -n p37 \
+          python=3.7 pandas numpy ipython
+}
+
+# C++ toolchain
+function install_cpp_toolchain() {
+    conda install -yq  \
+	  --file=$ARROW_DIR/ci/conda_env_cpp.yml \
+	  --file=$ARROW_DIR/ci/conda_env_gandiva.yml \
+      -n a37 -c conda-forge
+
+}
+
+# vim
+function install_vim() {
+    cp TOOLCHAIN_DIR/dotfiles/vimrc ~/.vimrc
+    mkdir -p ~/.vim/plugged
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+function install_arrow() {
+    pushd $CODE_DIR
+    git clone https://github.com/apache/arrow.git
+}
+
+install_dotfiles()
