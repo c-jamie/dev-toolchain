4fd998d3b3a761d69839d6aa871f5b9b43453713
Jamie C <44546398+c-jamie@users.noreply.github.com>
2022-12-02 17:52:29 +0000

    Update init.lua

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -5,16 +5,22 @@
 -- ========================================================================== --
 
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
 vim.opt.signcolumn = 'yes'
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime = 50
+
 
 -- Space as leader key
 vim.g.mapleader = ' '
@@ -94,6 +100,12 @@ require('packer').startup(function(use)
   use {'nvim-lualine/lualine.nvim'}
   use {'akinsho/bufferline.nvim'}
   use {'lukas-reineke/indent-blankline.nvim'}
+        
+  -- Motion
+  use {
+      "ggandor/leap.nvim",
+      config = function() require("leap").set_default_keymaps() end
+  }
 
   -- File explorer
   use {'kyazdani42/nvim-tree.lua'}
