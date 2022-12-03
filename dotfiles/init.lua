3e38e929465e6029d584c647761b01a0e6a1f861
c-jamie <jamie.b.clery@gmail.com>
2022-12-03 16:14:47 +0000

    fix: path

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -214,6 +214,14 @@ require('lualine').setup({
     component_separators = '|',
     section_separators = '',
   },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename', file_status = true, path = 2},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 })
 
 
