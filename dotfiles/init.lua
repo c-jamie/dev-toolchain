b9578d004f61922764e1bfef8a1e2fab8c5f3eee
c-jamie <jamie.b.clery@gmail.com>
2022-12-04 18:00:35 +0000

    feat: null-ls

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -691,22 +691,20 @@ require('mason-lspconfig').setup_handlers({
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-
   diagnostics_format = "[#{c}] #{m} (#{s})",
-
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-            vim.lsp.buf.formatting_sync()
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --           -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+  --           vim.lsp.buf.formatting_sync()
+  --       end,
+  --     })
+  --   end
+  -- end,
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.completion.spell,
