613887c0d39615070e614c7f92af18f058eed7f0
c-jamie <jamie.b.clery@gmail.com>
2022-12-04 17:54:48 +0000

    fix:  null-ls

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -533,6 +533,8 @@ require('mason-tool-installer').setup {
     'debugpy',
     'prettierd',
     'eslint_d',
+    'shfmt',
+    'fixjson',
   },
 
   -- if set to true this will check each tool for updates. If updates
@@ -683,15 +685,37 @@ require('mason-lspconfig').setup_handlers({
 })
 
 ---
--- eslint for js
+-- eslint for js + other formatters on save
 ---
 
 local null_ls = require("null-ls")
-
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.completion.spell,
-    },
+
+  diagnostics_format = "[#{c}] #{m} (#{s})"
+
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+            vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.completion.spell,
+
+    null_ls.builtins.formatters.prettierd,
+    null_ls.builtins.formatters.black,
+    null_ls.builtins.formatters.isort,
+
+    null_ls.builtins.formatters.shfmt,
+    null_ls.builtins.formatters.fixjson,
+  },
 })
