c81f1e8b813ef5f97eee4b2ddc836bbdaf2bbd6e
Jamie C <44546398+c-jamie@users.noreply.github.com>
2023-04-17 22:09:00 +0100

    Update init.lua

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -718,6 +718,7 @@ require('mason-lspconfig').setup_handlers({
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
+  debug = true,
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
