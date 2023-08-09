8e8d731f269c1aa18405784ec54ba2404b265c24
Jamie C <44546398+c-jamie@users.noreply.github.com>
2023-08-09 16:31:49 +0100

    Update init.lua

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -16,7 +16,7 @@ vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.signcolumn = 'yes'
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
